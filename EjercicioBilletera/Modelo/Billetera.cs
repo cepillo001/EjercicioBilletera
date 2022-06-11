2f78e92166cc60fd681b60a2880a7e1c5731eb8a
Jose Luis <Jose Luis@DESKTOP-QEA0B7Q>
2022-06-11 18:12:40 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioBilletera/Modelo/Billetera.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioBilletera.Modelo
+{
+    public class Billetera
+    {
+        //propiedades de la clase Billetera
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        //constructor de la clase Billetera
+        public Billetera()
+        {
+            Console.WriteLine("Se a creado una Billetera, vamos a llenarla: ");
+        }
+
+        //constructor que usa parametros
+        public Billetera(int billetesDe10, int billetesDe20, int billetesDe50, int billetesDe100, int billetesDe200, int billetesDe500, int billetesDe1000)
+        {
+            BilletesDe10 = billetesDe10;
+            BilletesDe20 = billetesDe20;
+            BilletesDe50 = billetesDe50;
+            BilletesDe100 = billetesDe100;
+            BilletesDe200 = billetesDe200;
+            BilletesDe500 = billetesDe500;
+            BilletesDe1000 = billetesDe1000;
+        }
+
+        //metodo para llenar la billetera creada (no devuelve nada, por eso el VOID)
+        public void LlenarBilletera()
+        {
+            Console.Write("Ingrese cuantos Billetes de $10 tiene: ");
+            BilletesDe10 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $20 tiene: ");
+            BilletesDe20 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $50 tiene: ");
+            BilletesDe50 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $100 tiene: ");
+            BilletesDe100 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $200 tiene: ");
+            BilletesDe200 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $500 tiene: ");
+            BilletesDe500 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese cuantos Billetes de $1000 tiene: ");
+            BilletesDe1000 = int.Parse(Console.ReadLine());
+        }
+
+        //metodo TOTAL (El tipo del metodo es un "decimal", porq devuelve un DECIMAL)
+        public decimal Total() 
+        {
+            decimal suma = 0;
+            suma = BilletesDe10 * 10 + BilletesDe20 * 20 + BilletesDe50 * 50 + BilletesDe100 * 100 + BilletesDe200 * 200 + BilletesDe500 * 500 + BilletesDe1000 * 1000;
+            return suma;
+        }
+
+        //metodo COMBINAR (El tipo del metodo es "Billetera" porq tiene que devolver una billetera)
+        public Billetera Combinar(Billetera segundaBilletera)
+        {
+            int SumaDe10 = BilletesDe10 + segundaBilletera.BilletesDe10;
+            int SumaDe20 = BilletesDe20 + segundaBilletera.BilletesDe20;
+            int SumaDe50 = BilletesDe50 + segundaBilletera.BilletesDe50;
+            int SumaDe100 = BilletesDe100 + segundaBilletera.BilletesDe100;
+            int SumaDe200 = BilletesDe200 + segundaBilletera.BilletesDe200;
+            int SumaDe500 = BilletesDe500 + segundaBilletera.BilletesDe500;
+            int SumaDe1000 = BilletesDe1000 + segundaBilletera.BilletesDe1000;
+
+            //uso el constructor que usa parametros para crear la 3er billetera y pasar los billetes de las 2 billeteras
+            var nuevaBilletera = new Billetera(SumaDe10, SumaDe20, SumaDe50, SumaDe100, SumaDe200, SumaDe500, SumaDe1000);
+            return nuevaBilletera;
+        }
+
+        //metodo que vacia Billeteras
+        public void VaciarBilletera() 
+        {
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+    }
+}
---
+++ EjercicioBilletera/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+BilletesDe10
+BilletesDe20
+BilletesDe50
+BilletesDe100
+BilletesDe200
+BilletesDe500
+BilletesDe1000
+
+Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total 
+en la billetera teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que 
+devuelva una nueva Billetera con la suma o combinacion del dinero de ambas billeteras.  
+
+Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras 
+deberan quedar en cero. (Sin billetes)
+*/
+
+using EjercicioBilletera.Modelo;
+
+var Billetera1 = new Billetera(); //creamos la primer billetera y la llenamos
+Billetera1.LlenarBilletera();
+
+Console.WriteLine();
+Console.WriteLine("En su Billetera usted tiene $" + Billetera1.Total());
+Console.WriteLine();
+
+var Billetera2 = new Billetera(); //creamos la segunda billetera y la llenamos
+Billetera2.LlenarBilletera();
+
+Console.WriteLine();
+Console.WriteLine("En su segunda Billetera usted tiene $" + Billetera2.Total());
+Console.WriteLine();
+
+//usando la Billetera1 llamo al metodo combinar y le paso la Billetera2 (como devuelve otra billetera, la guardo en Billetera3)
+Billetera Billetera3 = Billetera1.Combinar(Billetera2);
+
+Console.WriteLine("*** Todo el dinero de sus 2 Billeteras se junto en una Tercer Billetera que ahora tiene $" + Billetera3.Total() + " ***");
+Console.WriteLine();
+
+//vacio las billeteras
+Billetera1.VaciarBilletera();
+Billetera2.VaciarBilletera();
+
+Console.WriteLine($"*** En la Billetera1 hay $ {Billetera1.Total()}, en la Billetera2 hay $ {Billetera2.Total()} ***");
